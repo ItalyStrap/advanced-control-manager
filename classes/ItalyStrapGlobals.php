0378d94195a1708032c955836eea0b00aa68b764
Enea Overclokk <info@overclokk.net>
2015-02-26 14:03:18 +0100

    Update ItalyStrap

--- classes/ItalyStrapGlobals.php
+++ classes/ItalyStrapGlobals.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * This is a static class for append some inline javascript and print it in footer
+ * @link  http://coderrr.com/php-passing-data-between-classes/
+ */
 class ItalyStrapGlobals{
 
     public static $global = '';
