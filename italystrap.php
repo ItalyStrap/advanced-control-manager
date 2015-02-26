d94da8f4a5e7866686820a045fc6eca3ff21be20
Enea Overclokk <info@overclokk.net>
2015-02-26 13:01:13 +0100

    Update italystrap.php

--- italystrap.php
+++ italystrap.php
@@ -45,89 +45,76 @@ if ( !defined( 'ITALYSTRAP_BASENAME' ) )
 	define('ITALYSTRAP_BASENAME', plugin_basename( ITALYSTRAP_FILE ));
 
 /**
- * Load internationalization for plugin
- */
-function ItalyStrap_plugin_text_domain() {
-
-	load_plugin_textdomain( 'ItalyStrap', false, dirname( ITALYSTRAP_BASENAME ) . '/lang' );
-}
-add_action('init', 'ItalyStrap_plugin_text_domain');
-
-/**
  * Require PHP autoload
  */
 require(ITALYSTRAP_PLUGIN_PATH . 'vendor/autoload.php');
 
-// require(ITALYSTRAP_PLUGIN_PATH . 'classes/prova.php');
-
 /**
- * Load ItalyStrapAdmin() only if is admin
+ * Require Debug file
  */
-if ( is_admin() ) {
-
-	new ItalyStrapAdmin;
-	new ItalyStrapAdminMediaSettings;
-	new ItalyStrapAdminGallerySettings;
-}
-// else{
-// 	new ItalyStrapPluginInit();
-// }
-
+require(ITALYSTRAP_PLUGIN_PATH . 'debug/debug.php');
 
 /**
- * Load ItalyStrapCarouselLoader only if [gallery] shortcode exist
+ * Initialize class
  */
-function italystrap_load_carousel_shortcode() { //http://wordpress.stackexchange.com/questions/103549/wp-deregister-register-and-enqueue-dequeue
 
-	$post = get_post();
-	$gallery = false;
+if ( ! class_exists( 'ItalyStrapInit' ) ) {
 
-	if( isset($post->post_content) && has_shortcode( $post->post_content, 'gallery') )
-	        $gallery = true; // http://dannyvankooten.com/3935/only-load-contact-form-7-scripts-when-needed/
+	class ItalyStrapInit{
+		
+		public function __construct(){
 
-	if( !$gallery )
-	    new ItalyStrapCarouselLoader();
+			/**
+			 * Istantiate this class only if is admin
+			 */
+			if ( is_admin() ) {
 
+				new ItalyStrapAdmin;
+				new ItalyStrapAdminMediaSettings;
+				new ItalyStrapAdminGallerySettings;
+			}			
+
+			/**
+			 * adjust priority to make sure this runs
+			 */
+			add_action( 'init', array( $this, 'italystrap_init'), 100 );
+
+		}
+
+		/**
+		 * Init functions
+		 */
+		public function italystrap_init() {
+
+			/**
+			 * Load po file
+			 */
+			load_plugin_textdomain( 'ItalyStrap', false, dirname( ITALYSTRAP_BASENAME ) . '/lang' );
+
+			/**
+			 * Istantiate ItalyStrapCarouselLoader only if [gallery] shortcode exist
+			 * @link http://wordpress.stackexchange.com/questions/103549/wp-deregister-register-and-enqueue-dequeue
+			 */
+			$post = get_post();
+			$gallery = false;
+
+			if( isset($post->post_content) && has_shortcode( $post->post_content, 'gallery') )
+			        $gallery = true; // http://dannyvankooten.com/3935/only-load-contact-form-7-scripts-when-needed/
+
+			if( !$gallery )
+			    new ItalyStrapCarouselLoader();
+
+		}
+
+
+	}
+
+	new ItalyStrapInit;
 }
-// adjust priority to make sure this runs
-add_action( 'init', 'italystrap_load_carousel_shortcode', 100 );
 
 /**
- * Instatiate Mobile_Detect class for responive use
+ * Istantiate Mobile_Detect class for responive use
  * @todo Passare l'istanza dentro la classe http://stackoverflow.com/a/10634148
  * @var obj
  */
 $detect = new Mobile_Detect;
-
-
-// function prova(){
-// 	if ( defined('ITALYSTRAP_THEME') ){
-// 		do somethings
-// 	}
-// }
-// add_action( 'after_setup_theme', 'prova' );
-
-
-// http://codex.wordpress.org/Function_Reference/wp_get_theme
-// $my_theme = wp_get_theme( 'ItalyStrap' );
-// if ( $my_theme->exists() ) add some code
-// 
-// 
-
-// add_action( 'wp_footer', 'display_priority', 999 );
-// function display_priority(){
-// 	var_dump(ItalyStrapGlobals::get());
-// }
-
-/**
- * @link http://wordpress.stackexchange.com/questions/162862/wordpress-hooks-run-sequence
- * @link http://codex.wordpress.org/Plugin_API/Action_Reference
- * wp_footer (1)
- * wp_print_footer_scripts (1)
- * shutdown (1) 
- */
-// add_action( 'shutdown', function(){
-//     foreach( $GLOBALS['wp_actions'] as $action => $count )
-//         printf( '%s (%d) <br/>' . PHP_EOL, $action, $count );
-
-// });
