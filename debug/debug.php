9390b3b3453f602ab6c826a43c20ee38a00b413f
Enea Overclokk <info@overclokk.net>
2015-02-27 13:37:42 +0100

    Update debug.php

--- debug/debug.php
+++ debug/debug.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is only for debug purpose
+ * This file is only for internal use
  */
 
 // function prova(){
@@ -34,3 +34,21 @@
 	// foreach( $GLOBALS['wp_actions'] as $action => $count )
 	// 	printf( '%s (%d) <br/>' . PHP_EOL, $action, $count );
 // });
+
+
+/**
+ * Sperimentale, aggingereattributo style con altezza minima per quando
+ * si attiva il lazyloading sulle immagini
+ */
+// function kia_attachment_attributes( $attr ) {
+/**
+ * @todo $attr è un array con 3 valori
+ *       aggiungere un attributo style e dare min-height
+ *       con altezza presa dalla misura dell'immagine
+ *       Per esempio se è la misura media deve prendere il valore di 300px
+ *       Se questo sistema funziona ricordarsi di togliere lo stile dentro il carousel
+ */
+// 	// var_dump($attr);
+// 	return $attr;
+// }
+// add_filter('wp_get_attachment_image_attributes', 'kia_attachment_attributes', 10, 1);
