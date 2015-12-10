3b111f4d1bf83b87efe432406f5d3bc9cad5addd
overclokk <info@overclokk.net>
2015-12-10 23:42:24 +0100

    New Abstract Widget Class

--- widget/class-widget.php
+++ widget/class-widget.php
@@ -20,6 +20,608 @@ if ( ! defined( 'ITALYSTRAP_PLUGIN' ) or ! ITALYSTRAP_PLUGIN ) die();
 class Widget extends WP_Widget {
 
 	/**
+	 * Create Widget and call PHP5 constructor.
+	 * Creates a new widget and sets it's labels, description, fields and options.
+	 *
+	 * @access public
+	 * @param array $args The array with widget configuration.
+	 */
+	public function create_widget( $args ) {
+
+		/**
+		 * Settings some defaults options.
+		 * @var array
+		 */
+		$defaults = array(
+			'label'        => '',
+			'description'  => '',
+			'fields'       => array(),
+			'options'      => array(),
+		 );
+
+		/**
+		 * Parse and merge args with defaults.
+		 * @var array
+		 */
+		$args = wp_parse_args( $args, $defaults );
+
+		/**
+		 * Extract each arg to its own variable.
+		 * extract( $args, EXTR_SKIP );
+		 */
+
+		/**
+		 * Set the widget vars.
+		 */
+		$this->slug    = sanitize_title( $args['label'] );
+		$this->fields  = $args['fields'];
+
+		/**
+		 * Check options.
+		 * @var array
+		 */
+		$this->options = array( 'classname' => $this->slug, 'description' => $args['description'] );
+		if ( ! empty( $args['options'] ) ) $this->options = array_merge( $this->options, $args['options'] );
+
+		/**
+		 * Call WP_Widget to create the widget.
+		 */
+		parent::__construct( $this->slug, $args['label'], $this->options );
+
+	}
+
+	/**
+	 * Outputs the settings update form.
+	 *
+	 * @access public
+	 *
+	 * @param array $instance Current settings.
+	 */
+	public function form( $instance ) {
+
+		$this->instance = $instance;
+
+		/**
+		 * Creates the settings form.
+		 * @var string
+		 */
+		$form = $this->create_fields();
+
+		echo $form; // WPCS: XSS OK.
+
+	}
+
+	/**
+	 * Updates a particular instance of a widget.
+	 *
+	 * This function should check that `$new_instance` is set correctly. The newly-calculated
+	 * value of `$instance` should be returned. If false is returned, the instance won't be
+	 * saved/updated.
+	 *
+	 * @access public
+	 *
+	 * @param  array $new_instance New settings for this instance
+	 *                             as input by the user via WP_Widget::form().
+	 * @param  array $old_instance Old settings for this instance.
+	 * @return array Settings to save or bool false to cancel saving.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		$this->before_update_fields();
+
+		foreach ( $this->fields as $key ) {
+			$slug = $key['id'];
+
+			if ( isset( $key['validate'] ) ) {
+				if ( false === $this->validate( $key['validate'], $new_instance[ $slug ] ) )
+				return $instance;
+			}
+
+			if ( isset( $key['filter'] ) )
+				$instance[ $slug ] = $this->filter( $key['filter'], $new_instance[ $slug ] );
+			else $instance[ $slug ] = strip_tags( $new_instance[ $slug ] );
+		}
+
+		return $this->after_validate_fields( $instance );
+	}
+
+	/**
+	 * Fire Before Validate Fields
+	 *
+	 * Allows to hook code on the update.
+	 *
+	 * @access public
+	 */
+	public function before_update_fields() {
+		return;
+	}
+
+	/**
+	 * Fire After Validate Fields
+	 *
+	 * Allows to modify the output after validating the fields.
+	 *
+	 * @access public
+	 * @param  string $instance The $instance of widget.
+	 * @return string           Return the $instance of widget
+	 */
+	public function after_validate_fields( $instance = '' ) {
+		return $instance;
+	}
+
+	/**
+	 * Validate the value of key
+	 *
+	 * @access private
+	 * @param  string $rules Insert the rule name you want to use for validation.
+	 *                       Use | to separate more rules.
+	 * @param  string $value The value you want to validate.
+	 * @return string        Return the value validated
+	 */
+	private function validate( $rules, $value ) {
+		$rules = explode( '|', $rules );
+
+		if ( empty( $rules ) || count( $rules ) < 1 )
+			return true;
+
+		foreach ( $rules as $rule ) {
+			if ( false === $this->do_validation( $rule, $value ) )
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Filter the value of key
+	 *
+	 * @access private
+	 * @param  string $filters Insert the filter name you want to use.
+	 *                         Use | to separate more filter.
+	 * @param  string $value   The value you want to filter.
+	 * @return string          Return the value filtered
+	 */
+	private function filter( $filters, $value ) {
+		$filters = explode( '|', $filters );
+
+		if ( empty( $filters ) || count( $filters ) < 1 )
+			return $value;
+
+		foreach ( $filters as $filter )
+			$value = $this->do_filter( $filter, $value );
+
+		return $value;
+	}
+
+	/**
+	 * Validate the value of key
+	 *
+	 * @access private
+	 * @param  string $rule  Insert the rule name you want to use for validation.
+	 * @param  string $value The value you want to validate.
+	 * @return string        Return the value validated
+	 */
+	private function do_validation( $rule, $value = '' ) {
+		switch ( $rule ) {
+
+			case 'alpha':
+				return ctype_alpha( $value );
+			break;
+
+			case 'alpha_numeric':
+				return ctype_alnum( $value );
+			break;
+
+			case 'alpha_dash':
+				return preg_match( '/^[a-z0-9-_]+$/', $value );
+			break;
+
+			case 'numeric':
+				return ctype_digit( $value );
+			break;
+
+			case 'integer':
+				return (bool) preg_match( '/^[\-+]?[0-9]+$/', $value );
+			break;
+
+			case 'boolean':
+				return is_bool( $value );
+			break;
+
+			case 'email':
+				return is_email( $value );
+			break;
+
+			case 'decimal':
+				return (bool) preg_match( '/^[\-+]?[0-9]+\.[0-9]+$/', $value );
+			break;
+
+			case 'natural':
+				return (bool) preg_match( '/^[0-9]+$/', $value );
+			return;
+
+			case 'natural_not_zero':
+				if ( ! preg_match( '/^[0-9]+$/', $value ) ) return false;
+				if ( 0 === $value ) return false;
+				return true;
+			return;
+
+			default:
+				if ( method_exists( $this, $rule ) )
+					return $this->$rule( $value );
+				else return false;
+			break;
+
+		}
+	}
+
+	/**
+	 * Filter the value of key
+	 *
+	 * @access private
+	 * @param  string $filter The filter name you want to use.
+	 * @param  string $value  The value you want to filter.
+	 * @return string         Return the value filtered
+	 */
+	private function do_filter( $filter, $value = '' ) {
+		switch ( $filter ) {
+
+			case 'strip_tags':
+				return strip_tags( $value );
+			break;
+
+			case 'wp_strip_all_tags':
+				return wp_strip_all_tags( $value );
+			break;
+
+			case 'esc_attr':
+				return esc_attr( $value );
+			break;
+
+			case 'esc_url':
+				return esc_url( $value );
+			break;
+
+			case 'esc_textarea':
+				return esc_textarea( $value );
+			break;
+
+			default:
+				if ( method_exists( $this, $filter ) )
+					return $this->$filter( $value );
+				else return $value;
+			break;
+		}
+	}
+
+	/**
+	 * Create Fields
+	 *
+	 * Creates each field defined.
+	 *
+	 * @access protected
+	 * @param  string $out The HTML form output.
+	 * @return string      Return the HTML Fields
+	 */
+	protected function create_fields( $out = '' ) {
+
+		$out = $this->before_create_fields( $out );
+
+		if ( ! empty( $this->fields ) ) {
+			foreach ( $this->fields as $key )
+				$out .= $this->create_field( $key );
+		}
+
+		$out = $this->after_create_fields( $out );
+
+		return $out;
+	}
+
+	/**
+	 * Fire Before Create Fields
+	 *
+	 * Allows to modify code before creating the fields.
+	 *
+	 * @access protected
+	 * @param  string $out The HTML form output.
+	 * @return string      Return the HTML Fields
+	 */
+	protected function before_create_fields( $out = '' ) {
+		return $out;
+	}
+
+	/**
+	 * Fire After Create Fields
+	 *
+	 * Allows to modify code after creating the fields.
+	 *
+	 * @access protected
+	 * @param  string $out The HTML form output.
+	 * @return string      Return the HTML Fields
+	 */
+	protected function after_create_fields( $out = '' ) {
+		return $out;
+	}
+
+	/**
+	 * Create the Fields
+	 *
+	 * @access protected
+	 * @param  array  $key The key of field's array to create the HTML field.
+	 * @param  string $out The HTML form output.
+	 * @return string      Return the HTML Fields
+	 */
+	protected function create_field( $key, $out = '' ) {
+
+		/* Set Defaults */
+		$key['std'] = isset( $key['std'] ) ? $key['std'] : '';
+
+		$slug = $key['id'];
+
+		if ( isset( $this->instance[ $slug ] ) )
+			$key['value'] = empty( $this->instance[ $slug ] ) ? '' : strip_tags( $this->instance[ $slug ] );
+		else unset( $key['value'] );
+
+		/* Set field id and name  */
+		$key['_id'] = $this->get_field_id( $slug );
+		$key['_name'] = $this->get_field_name( $slug );
+
+		/* Set field type */
+		if ( ! isset( $key['type'] ) ) $key['type'] = 'text';
+
+		/* Prefix method */
+		$field_method = 'create_field_' . str_replace( '-', '_', $key['type'] );
+
+		/* Check for <p> Class */
+		$p = ( isset( $key['class-p'] ) ) ? '<p class="'.$key['class-p'].'">' : '<p>';
+
+		/* Run method */
+		if ( method_exists( $this, $field_method ) )
+			return $p.$this->$field_method( $key ).'</p>';
+
+	}
+
+	/**
+	 * Create the Field Text
+	 *
+	 * @access protected
+	 * @param  array  $key The key of field's array to create the HTML field.
+	 * @param  string $out The HTML form output.
+	 * @return string      Return the HTML Field Text
+	 */
+	protected function create_field_text( $key, $out = '' ) {
+		$out .= $this->create_field_label( $key['name'], $key['_id'] ) . '<br/>';
+
+		$out .= '<input type="text" ';
+
+		if ( isset( $key['class'] ) )
+			$out .= 'class="' . esc_attr( $key['class'] ) . '" ';
+
+		$value = isset( $key['value'] ) ? $key['value'] : $key['std'];
+
+		$out .= 'id="' . esc_attr( $key['_id'] ) . '" name="' . esc_attr( $key['_name'] ) . '" value="' . esc_attr__( $value ) . '" ';
+
+		if ( isset( $key['size'] ) )
+			$out .= 'size="' . esc_attr( $key['size'] ) . '" ';
+
+		$out .= ' />';
+
+		if ( isset( $key['desc'] ) )
+			$out .= $this->create_field_description( $key['desc'] );
+
+		return $out;
+	}
+
+	/**
+	 * Create the Field Textarea
+	 *
+	 * @access protected
+	 * @param  array  $key The key of field's array to create the HTML field.
+	 * @param  string $out The HTML form output.
+	 * @return string      Return the HTML Field Textarea
+	 */
+	protected function create_field_textarea( $key, $out = '' ) {
+		$out .= $this->create_field_label( $key['name'], $key['_id'] ) . '<br/>';
+
+		$out .= '<textarea ';
+
+		if ( isset( $key['class'] ) )
+			$out .= 'class="' . esc_attr( $key['class'] ) . '" ';
+
+		if ( isset( $key['rows'] ) )
+			$out .= 'rows="' . esc_attr( $key['rows'] ) . '" ';
+
+		if ( isset( $key['cols'] ) )
+			$out .= 'cols="' . esc_attr( $key['cols'] ) . '" ';
+
+		$value = isset( $key['value'] ) ? $key['value'] : $key['std'];
+
+		$out .= 'id="'. esc_attr( $key['_id'] ) .'" name="' . esc_attr( $key['_name'] ) . '">'.esc_html( $value );
+
+		$out .= '</textarea>';
+
+		if ( isset( $key['desc'] ) )
+			$out .= $this->create_field_description( $key['desc'] );
+
+		return $out;
+	}
+
+	/**
+	 * Create the Field Checkbox
+	 *
+	 * @access protected
+	 * @param  array  $key The key of field's array to create the HTML field.
+	 * @param  string $out The HTML form output.
+	 * @return string      Return the HTML Field Checkbox
+	 */
+	protected function create_field_checkbox( $key, $out = '' ) {
+		$out .= $this->create_field_label( $key['name'], $key['_id'] );
+
+		$out .= ' <input type="checkbox" ';
+
+		if ( isset( $key['class'] ) )
+			$out .= 'class="' . esc_attr( $key['class'] ) . '" ';
+
+		$out .= 'id="' . esc_attr( $key['_id'] ) . '" name="' . esc_attr( $key['_name'] ) . '" value="1" ';
+
+		if ( ( isset( $key['value'] ) && 1 === $key['value'] ) || ( ! isset( $key['value'] ) && 1 === $key['std'] ) )
+			$out .= ' checked="checked" ';
+
+		$out .= ' /> ';
+
+		if ( isset( $key['desc'] ) )
+			$out .= $this->create_field_description( $key['desc'] );
+
+		return $out;
+	}
+
+	/**
+	 * Create the Field Select
+	 *
+	 * @access protected
+	 * @param  array  $key The key of field's array to create the HTML field.
+	 * @param  string $out The HTML form output.
+	 * @return string      Return the HTML Field Select
+	 */
+	protected function create_field_select( $key, $out = '' ) {
+		$out .= $this->create_field_label( $key['name'], $key['_id'] ) . '<br/>';
+
+		$out .= '<select id="' . esc_attr( $key['_id'] ) . '" name="' . esc_attr( $key['_name'] ) . '" ';
+
+		if ( isset( $key['class'] ) )
+			$out .= 'class="' . esc_attr( $key['class'] ) . '" ';
+
+		$out .= '> ';
+
+		$selected = isset( $key['value'] ) ? $key['value'] : $key['std'];
+
+		foreach ( $key['fields'] as $field => $option ) {
+
+			$out .= '<option value="' . esc_attr__( $option['value'] ) . '" ';
+
+			if ( esc_attr( $selected ) === $option['value'] )
+				$out .= ' selected="selected" ';
+
+			$out .= '> '.esc_html( $option['name'] ).'</option>';
+
+		}
+
+		$out .= ' </select> ';
+
+		if ( isset( $key['desc'] ) )
+			$out .= $this->create_field_description( $key['desc'] );
+
+		return $out;
+	}
+
+	/**
+	 * Create the Field Select with Options Group
+	 *
+	 * @access protected
+	 * @param  array  $key The key of field's array to create the HTML field.
+	 * @param  string $out The HTML form output.
+	 * @return string      Return the HTML Field Select with Options Group
+	 */
+	protected function create_field_select_group( $key, $out = '' ) {
+
+		$out .= $this->create_field_label( $key['name'], $key['_id'] ) . '<br/>';
+
+		$out .= '<select id="' . esc_attr( $key['_id'] ) . '" name="' . esc_attr( $key['_name'] ) . '" ';
+
+		if ( isset( $key['class'] ) )
+			$out .= 'class="' . esc_attr( $key['class'] ) . '" ';
+
+		$out .= '> ';
+
+		$selected = isset( $key['value'] ) ? $key['value'] : $key['std'];
+
+		foreach ( $key['fields'] as $group => $fields ) {
+
+			$out .= '<optgroup label="' . $group . '">';
+
+			foreach ( $fields as $field => $option ) {
+
+				$out .= '<option value="' . esc_attr( $option['value'] ) . '" ';
+
+				if ( esc_attr( $selected ) === $option['value'] )
+					$out .= ' selected="selected" ';
+
+				$out .= '> ' . esc_html( $option['name'] ) . '</option>';
+			}
+
+			$out .= '</optgroup>';
+
+		}
+
+		$out .= '</select>';
+
+		if ( isset( $key['desc'] ) )
+			$out .= $this->create_field_description( $key['desc'] );
+
+		return $out;
+	}
+
+	/**
+	 * Create the field number
+	 *
+	 * @access protected
+	 * @param  array  $key The key of field's array to create the HTML field.
+	 * @param  string $out The HTML form output.
+	 * @return string      Return the HTML field number
+	 */
+	protected function create_field_number( $key, $out = '' ) {
+
+		$out .= $this->create_field_label( $key['name'], $key['_id'] ) . '<br/>';
+
+		$out .= '<input type="number" ';
+
+		if ( isset( $key['class'] ) )
+			$out .= 'class="' . esc_attr( $key['class'] ) . '" ';
+
+		$value = isset( $key['value'] ) ? $key['value'] : $key['std'];
+
+		$out .= 'id="' . esc_attr( $key['_id'] ) . '" name="' . esc_attr( $key['_name'] ) . '" value="' . esc_attr__( $value ) . '" ';
+
+		if ( isset( $key['size'] ) )
+			$out .= 'size="' . esc_attr( $key['size'] ) . '" ';
+
+		$out .= ' />';
+
+		if ( isset( $key['desc'] ) )
+			$out .= $this->create_field_description( $key['desc'] );
+
+		return $out;
+	}
+
+	/**
+	 * Create the field description
+	 *
+	 * @access protected
+	 * @param  string $desc The description.
+	 * @return string       Return the description
+	 */
+	protected function create_field_description( $desc ) {
+
+		return  '<br/><small class="description">' . esc_html( $desc ) . '</small>';
+
+	}
+
+	/**
+	 * Create the field label
+	 *
+	 * @access protected
+	 * @param  string $name The labels name.
+	 * @param  string $id   The labels ID.
+	 * @return string       Return the labels
+	 */
+	protected function create_field_label( $name = '', $id = '' ) {
+
+		return '<label for="' . esc_attr( $id ). '">' . esc_html( $name ) . ':</label>';
+
+	}
+
+	/**
 	 * Upload the Javascripts for the media uploader in widget config
 	 *
 	 * @param string $hook The name of the page.
