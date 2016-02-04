4c9c0cc5d5e6af9509d689797cbe6ba2fbdd4f00
overclokk <info@overclokk.net>
2016-02-05 00:28:05 +0100

    Deprecated Class

---
+++ deprecated/ItalyStrapCarousel.php
@@ -0,0 +1,36 @@
+<?php namespace ItalyStrap\Core;
+/**
+ * ItalyStrap Carousel initially forked from Agnosia Bootstrap Carousel by AuSoft
+ *
+ * Display a Bootstrap Carousel based on selected images and their titles and
+ * descriptions. You need to include the Bootstrap CSS and Javascript files on
+ * your own; otherwise the class will not work.
+ *
+ * @todo https://codex.wordpress.org/it:Shortcode_Gallery Aggiungere parametri mancanti
+ *
+ * @package ItalyStrap
+ * @version 1.0
+ * @since   1.0
+ *
+ * @deprecated 2.0 Class deprecated use ItalyStrap\Core\Carousel_Bootstrap
+ */
+if ( ! class_exists( 'ItalyStrapCarousel' ) ) {
+
+	/**
+	 * The Carousel Bootsrap class
+	 */
+	class ItalyStrapCarousel extends Carousel {
+
+		/**
+		 * Initialize the contstructor
+		 * @param array $args The carousel attribute.
+		 */
+		function __construct( $args ) {
+
+			_deprecated_function( __CLASS__, '2.0', 'ItalyStrap\\\Core\\\Carousel_Bootstrap' );
+
+			parent::__construct( $args );
+
+		}
+	}
+}
