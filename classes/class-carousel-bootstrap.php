26d34efd023c403d3470ef3a1fcf7b413fc12ed7
overclokk <info@overclokk.net>
2016-02-05 00:28:58 +0100

    New Class for carousel

---
+++ classes/class-carousel-bootstrap.php
@@ -0,0 +1,30 @@
+<?php namespace ItalyStrap\Core;
+/**
+ * ItalyStrap Carousel initially forked from Agnosia Bootstrap Carousel by AuSoft
+ *
+ * Display a Bootstrap Carousel based on selected images and their titles and
+ * descriptions. You need to include the Bootstrap CSS and Javascript files on
+ * your own; otherwise the class will not work.
+ *
+ * @todo https://codex.wordpress.org/it:Shortcode_Gallery Aggiungere parametri mancanti
+ *
+ * @package ItalyStrap\Core
+ * @version 1.0
+ * @since   2.0
+ */
+
+/**
+ * The Carousel Bootsrap class
+ */
+class Carousel_Bootstrap extends Carousel {
+
+	/**
+	 * Initialize the contstructor
+	 * @param array $args The carousel attribute.
+	 */
+	function __construct( $args ) {
+
+		parent::__construct( $args );
+
+	}
+}
