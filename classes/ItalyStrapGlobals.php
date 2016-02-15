4acc747318569b75489bbe40523447e1d88c2c9b
overclokk <info@overclokk.net>
2016-02-15 11:26:35 +0100

    Added comment to file head

--- classes/ItalyStrapGlobals.php
+++ classes/ItalyStrapGlobals.php
@@ -1,4 +1,15 @@
-<?php
+<?php namespace ItalyStrap\Core;
+/**
+ * Init API: Init Class
+ *
+ * @package ItalyStrap
+ * @since 2.0.0
+ */
+
+if ( ! defined( 'ITALYSTRAP_PLUGIN' ) or ! ITALYSTRAP_PLUGIN ) {
+    die();
+}
+
 /**
  * This is a static class for append some inline javascript and print it in footer
  * @link  http://coderrr.com/php-passing-data-between-classes/
