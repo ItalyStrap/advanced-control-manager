1a29ac3a736057af1c71734bda31b1a0adf2f10b
overclokk <info@overclokk.net>
2016-02-15 11:21:39 +0100

    New Init Class

---
+++ class-init.php
@@ -0,0 +1,179 @@
+<?php namespace ItalyStrap\Core;
+/**
+ * Init API: Init Class
+ *
+ * @package ItalyStrap
+ * @since 2.0.0
+ */
+
+if ( ! defined( 'ITALYSTRAP_PLUGIN' ) or ! ITALYSTRAP_PLUGIN ) {
+	die();
+}
+
+/**
+ * Init Class for ItalyStrap Core
+ */
+class Init {
+
+	/**
+	 * The plugin's options
+	 *
+	 * @var string
+	 */
+	private $options = '';
+
+	/**
+	 * Fire the construct
+	 */
+	public function __construct() {
+
+		$this->options = get_option( 'italystrap_settings' );
+
+		/**
+		 * Add ID to post_type table
+		 */
+		require( 'hooks/simply-show-ids.php' );
+		add_action( 'admin_init', '\ItalyStrap\Admin\ssid_add' );
+
+		/**
+		 * Adjust priority to make sure this runs
+		 */
+		add_action( 'init', array( $this, 'on_load' ), 100 );
+
+		/**
+		 * Print inline css in header
+		 */
+		add_action( 'wp_head', array( $this, 'print_inline_css_in_header' ), 999 );
+
+		/**
+		 * Print inline script in footer
+		 * Load after all and before shotdown hook
+		 */
+		add_action( 'wp_print_footer_scripts', array( $this, 'print_inline_script_in_footer' ), 999 );
+
+		if ( isset( $this->options['lazyload'] ) && ! is_admin() ) {
+			ItalyStrapLazyload::init();
+		}
+
+		add_filter( 'widget_title', 'ItalyStrap\Core\render_html_in_title_output' );
+		add_filter( 'the_title', 'ItalyStrap\Core\render_html_in_title_output' );
+
+		add_filter( 'mobile_detect', 'ItalyStrap\Core\new_mobile_detect' );
+
+	}
+
+	/**
+	 * Init functions
+	 */
+	public function on_load() {
+
+		/**
+		 * Load po file
+		 */
+		load_plugin_textdomain( 'ItalyStrap', false, dirname( ITALYSTRAP_BASENAME ) . '/lang' );
+
+		/**
+		 * Istantiate ItalyStrapCarouselLoader only if [gallery] shortcode exist
+		 *
+		 * @link http://wordpress.stackexchange.com/questions/103549/wp-deregister-register-and-enqueue-dequeue
+		 */
+		$post = get_post();
+		$gallery = false;
+
+		if ( isset( $post->post_content ) && has_shortcode( $post->post_content, 'gallery' ) ) {
+			$gallery = true; // A http://dannyvankooten.com/3935/only-load-contact-form-7-scripts-when-needed/ .
+		}
+
+		if ( ! $gallery ) {
+			new \ItalyStrap\Core\ItalyStrapCarouselLoader();
+		}
+
+	}
+
+	/**
+	 * Add action to widget_init
+	 * Initialize widget
+	 */
+	public function widgets_init() {
+
+		add_action( 'widgets_init', function() {
+
+			if ( isset( $this->options['vcardwidget'] ) ) {
+				register_widget( 'ItalyStrap\Core\Vcard_Widget' );
+			}
+
+			if ( isset( $this->options['post_widget'] ) ) {
+				register_widget( 'ItalyStrap\Core\Widget_Posts' );
+			}
+
+			if ( isset( $this->options['media_widget'] ) ) {
+				register_widget( 'ItalyStrap\Core\Widget_Media_Carousel' );
+			}
+
+			register_widget( 'ItalyStrap\Core\Widget_Breadcrumbs' );
+			register_widget( 'ItalyStrap\Core\Widget_VCard' );
+			register_widget( 'ItalyStrap\Core\Widget_Posts2' );
+			register_widget( 'ItalyStrap\Core\Widget_Image' );
+		});
+
+	}
+
+	/**
+	 * Print inline script in footer after all and before shotdown hook.
+	 *
+	 * @todo Creare un sistema che appenda regolarmente dopo gli script
+	 *       e tenga presente delle dipendenze da jquery
+	 */
+	public function print_inline_script_in_footer() {
+
+		$scipt = ItalyStrapGlobals::get();
+
+		if ( $scipt ) { echo '<script type="text/javascript">/*<![CDATA[*/' . esc_js( $scipt ) . '/*]]>*/</script>';
+		} else { echo ''; }
+
+	}
+
+	/**
+	 * Print inline css.
+	 */
+	public function print_inline_css_in_header() {
+
+		$css = ItalyStrapGlobalsCss::get();
+
+		if ( $css ) { echo '<style>' . esc_attr( $css ) . '</style>';
+		} else { echo ''; }
+	}
+} // End Init
+
+/**
+ * New instance of Italystrap Init
+ *
+ * @var Init
+ */
+$italystrap_init = new Init;
+
+/**
+ * Register widget
+ */
+$italystrap_init->widgets_init();
+
+/**
+ * Istantiate this class only if is admin
+ */
+if ( is_admin() ) {
+
+	new \ItalyStrapAdmin;
+
+	new \ItalyStrapAdminGallerySettings;
+
+	/**
+	 * Da testare
+	 *
+	 * Example: new \ItalyStrap\Admin\Gallery_Settings;
+	 * $gallery_settings = new Gallery_Settings;
+	 * add_action( 'admin_init', array( $gallery_settings, 'admin_init' ) );
+	 */
+
+	$image_size_media = new \ItalyStrapAdminMediaSettings;
+	add_filter( 'image_size_names_choose', array( $image_size_media, 'get_image_sizes' ), 999 );
+}
--- italystrap.php
+++ italystrap.php
@@ -15,15 +15,6 @@
  * @since 1.0.0
  */
 
-use \ItalyStrap\Core\Widget_Posts;
-use \ItalyStrap\Core\Widget_Media_Carousel;
-use \ItalyStrap\Core\Vcard_Widget;
-use \ItalyStrap\Core\ItalyStrapCarouselLoader;
-use \ItalyStrap\Core\Carousel_Bootstrap;
-use \ItalyStrap\Core\Query_Posts;
-
-// use \ItalyStrap\Admin\Gallery_Settings;
-
 /**
  * This will make shure the plugin files can't be accessed within the web browser directly.
  */
@@ -34,32 +25,37 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * Define some costant for internal use
  */
-if ( ! defined( 'ITALYSTRAP_PLUGIN' ) )
+if ( ! defined( 'ITALYSTRAP_PLUGIN' ) ) {
 	define( 'ITALYSTRAP_PLUGIN', true );
+}
 
 /**
  * Example = F:\xampp\htdocs\italystrap\wp-content\plugins\italystrap-extended\italystrap.php
  */
-if ( ! defined( 'ITALYSTRAP_FILE' ) )
+if ( ! defined( 'ITALYSTRAP_FILE' ) ) {
 	define( 'ITALYSTRAP_FILE', __FILE__ );
+}
 
 /**
  * Example = F:\xampp\htdocs\italystrap\wp-content\plugins\italystrap-extended/
  */
-if ( ! defined( 'ITALYSTRAP_PLUGIN_PATH' ) )
+if ( ! defined( 'ITALYSTRAP_PLUGIN_PATH' ) ) {
 	define( 'ITALYSTRAP_PLUGIN_PATH', plugin_dir_path( ITALYSTRAP_FILE ) );
+}
 
 /**
  * Example: 'http://192.168.1.10/italystrap/wp-content/plugins/italystrap-extended/'
  */
-if ( ! defined( 'ITALYSTRAP_PLUGIN_URL' ) )
+if ( ! defined( 'ITALYSTRAP_PLUGIN_URL' ) ) {
 	define( 'ITALYSTRAP_PLUGIN_URL', plugin_dir_url( ITALYSTRAP_FILE ) );
+}
 
 /**
  * Example = italystrap-extended/italystrap.php
  */
-if ( ! defined( 'ITALYSTRAP_BASENAME' ) )
+if ( ! defined( 'ITALYSTRAP_BASENAME' ) ) {
 	define( 'ITALYSTRAP_BASENAME', plugin_basename( ITALYSTRAP_FILE ) );
+}
 
 /**
  * Require PHP autoload
@@ -67,260 +63,18 @@ if ( ! defined( 'ITALYSTRAP_BASENAME' ) )
 require( ITALYSTRAP_PLUGIN_PATH . 'vendor/autoload.php' );
 
 /**
- * Require Debug file
+ * Load general function before init
  */
-if ( is_admin() ) require( ITALYSTRAP_PLUGIN_PATH . 'debug/debug.php' );
-
 require( ITALYSTRAP_PLUGIN_PATH . 'functions/general-functions.php' );
 
 /**
- * Initialize class
+ * Init the plugin
  */
-
-if ( ! class_exists( 'ItalyStrapInit' ) ) {
-
-	/**
-	 * Init Class fo r ItalyStrap core
-	 */
-	class ItalyStrapInit {
-
-		/**
-		 * The plugin's options
-		 * @var string
-		 */
-		private $options = '';
-
-		/**
-		 * Fire the construct
-		 */
-		public function __construct() {
-
-			$this->options = get_option( 'italystrap_settings' );
-
-			/**
-			 * Istantiate this class only if is admin
-			 */
-			if ( is_admin() ) {
-
-				new ItalyStrapAdmin;
-
-				new ItalyStrapAdminGallerySettings;
-				// new \ItalyStrap\Admin\Gallery_Settings;
-				// $gallery_settings = new Gallery_Settings;
-				// add_action( 'admin_init', array( $gallery_settings, 'admin_init' ) );
-
-				$image_size_media = new ItalyStrapAdminMediaSettings;
-				add_filter( 'image_size_names_choose', array( $image_size_media, 'get_image_sizes' ), 999 );
-			}
-
-			/**
-			 * Add ID to post_type table
-			 */
-			require( 'hooks/simply-show-ids.php' );
-			add_action( 'admin_init', '\ItalyStrap\Admin\ssid_add' );
-
-			/**
-			 * Adjust priority to make sure this runs
-			 */
-			add_action( 'init', array( $this, 'italystrap_init' ), 100 );
-
-			/**
-			 * Print inline css in header
-			 */
-			add_action( 'wp_head', array( $this, 'italystrap_print_inline_css_in_header' ), 999 );
-
-			/**
-			 * Print inline script in footer
-			 * Load after all and before shotdown hook
-			 */
-			add_action( 'wp_print_footer_scripts', array( $this, 'italystrap_print_inline_script_in_footer' ), 999 );
-
-			if ( isset( $this->options['lazyload'] ) && ! is_admin() )
-				ItalyStrapLazyload::init();
-
-			if ( isset( $this->options['vcardwidget'] ) )
-				add_action( 'widgets_init', function() {
-					register_widget( 'ItalyStrap\Core\Vcard_Widget' );
-				});
-
-			if ( isset( $this->options['post_widget'] ) )
-				add_action( 'widgets_init', function() {
-					register_widget( 'ItalyStrap\Core\Widget_Posts' );
-				});
-
-			if ( isset( $this->options['media_widget'] ) )
-				add_action( 'widgets_init', function() {
-					register_widget( 'ItalyStrap\Core\Widget_Media_Carousel' );
-				});
-
-				add_action( 'widgets_init', function() {
-					register_widget( 'ItalyStrap\Core\Widget_Breadcrumbs' );
-					register_widget( 'ItalyStrap\Core\Widget_VCard' );
-					register_widget( 'ItalyStrap\Core\Widget_Posts2' );
-					register_widget( 'ItalyStrap\Core\Widget_Image' );
-					add_filter( 'widget_title', 'html_widget_title_replace' );
-				});
-
-		}
-
-		/**
-		 * Init functions
-		 */
-		public function italystrap_init() {
-
-			/**
-			 * Load po file
-			 */
-			load_plugin_textdomain( 'ItalyStrap', false, dirname( ITALYSTRAP_BASENAME ) . '/lang' );
-
-			/**
-			 * Istantiate ItalyStrapCarouselLoader only if [gallery] shortcode exist
-			 * @link http://wordpress.stackexchange.com/questions/103549/wp-deregister-register-and-enqueue-dequeue
-			 */
-			$post = get_post();
-			$gallery = false;
-
-			if ( isset( $post->post_content ) && has_shortcode( $post->post_content, 'gallery' ) )
-				$gallery = true; // A http://dannyvankooten.com/3935/only-load-contact-form-7-scripts-when-needed/ .
-
-			if ( ! $gallery )
-				new \ItalyStrap\Core\ItalyStrapCarouselLoader();
-
-		}
-
-		/**
-		 * Print inline script in footer after all and before shotdown hook.
-		 *
-		 * @todo Creare un sistema che appenda regolarmente dopo gli script
-		 *       e tenga presente delle dipendenze da jquery
-		 */
-		public function italystrap_print_inline_script_in_footer() {
-
-			$scipt = ItalyStrapGlobals::get();
-
-			if ( $scipt ) echo '<script type="text/javascript">/*<![CDATA[*/' . $scipt . '/*]]>*/</script>';
-
-			else echo '';
-
-		}
-
-		/**
-		 * Print inline css.
-		 */
-		public function italystrap_print_inline_css_in_header() {
-
-			$css = ItalyStrapGlobalsCss::get();
-
-			if ( $css ) echo '<style>' . $css . '</style>';
-
-			else echo '';
-		}
-	} // End ItalyStrapInit
-
-	new ItalyStrapInit;
-}
+require( ITALYSTRAP_PLUGIN_PATH . 'class-init.php' );
 
 /**
- * Istantiate Mobile_Detect class for responive use
- * @todo Passare l'istanza dentro la classe http://stackoverflow.com/a/10634148
- * @var obj
+ * Require Debug file, this file is only for internal development
  */
-// $detect = new \Detection\MobileDetect;
-$detect = new Mobile_Detect;
-
-/**
- * Function from HTML Widget Text plugins
- * @author Jaimyn Mayer https://jabelone.com.au
- */
-// add_filter( 'widget_title', 'html_widget_title_replace' ); //Uses the built in filter function.  The title of the widget is passed to the function.
-
-function html_widget_title_replace( $widget_title ) {
-
-	$tagopen = '['; //Our HTML opening tag replacement
-	$tagclose = ']'; //Our HTML closing tag replacement
-
-	$widget_title = str_replace( $tagopen, '<', $widget_title );
-	$widget_title = str_replace( $tagclose, '>', $widget_title );
-
-	return $widget_title;
+if ( defined( 'ITALYSTRAP_DEV' ) ) {
+	require( ITALYSTRAP_PLUGIN_PATH . 'debug/debug.php' );
 }
-
-
-function test_carousel_posts() {
-
-	$atts = array();
-
-	$atts['ids'] = '1045,2051,13,12,1177,16,1163';
-	$atts['type'] = 'carousel';
-	$atts['name'] = 'prova';
-	$atts['size'] = 'large';
-
-	// $atts['ids'] = array(
-	// 	'id'	=> 'ids',
-	// 	'default'	=> '1777,1016,1011',
-	// 	);
-	// $atts['type'] = array(
-	// 	'id'		=> 'type',
-	// 	'default'	=> 'carousel',
-	// 	);
-
-	// 'ids'				=> array(
-	// 			'name'		=> __( 'Images ID', 'ItalyStrap' ),
-	// 			'desc'		=> __( 'Enter the image ID.', 'ItalyStrap' ),
-	// 			'id'		=> 'ids',
-	// 			'type'		=> 'media_list',
-	// 			'class'		=> 'widefat ids',
-	// 			'default'	=> false,
-	// 			// 'validate'	=> 'numeric_comma',
-	// 			'filter'	=> 'sanitize_text_field',
-	// 			 ),
-
-	// /**
-	//  * Type of gallery. If it's not "carousel", nothing will be done.
-	//  */
-	// 'type'				=> array(
-	// 			'name'		=> __( 'Type of gallery', 'ItalyStrap' ),
-	// 			'desc'		=> __( 'Enter the type of gallery, if it\'s not "carousel", nothing will be done.', 'ItalyStrap' ),
-	// 			'id'		=> 'type',
-	// 			'type'		=> 'select',
-	// 			'class'		=> 'widefat',
-	// 			'class-p'	=> 'hidden',
-	// 			'default'	=> 'carousel',
-	// 			'options'	=> array(
-	// 						'standard'  => __( 'Standard Gallery', 'ItalyStrap' ),
-	// 						'carousel'  => __( 'Carousel (Default)', 'ItalyStrap' ),
-	// 			 			),
-	// 			'validate'	=> 'alpha_numeric',
-	// 			'filter'	=> 'sanitize_text_field',
-	// 			 ),
-
-	$carousel_posts = new \ItalyStrap\Core\Carousel_Bootstrap( $atts );
-	// var_dump( $carousel_posts->validate_data() );
-	var_dump( $carousel_posts->__get( 'output' ) );
-	echo $carousel_posts->__get( 'output' );
-
-}
-// add_action( 'content_container_open', 'test_carousel_posts' );
-// add_action( 'single', 'test_carousel_posts' );
-
-// Add Shortcode
-function query_posts_shortcode( $atts , $content = null ) {
-
-	// Attributes
-	// extract( shortcode_atts(
-	// 	array(
-	// 		'posts' => '5',
-	// 	), $atts )
-	// );
-
-	// Code
-// $output = '<ul>';
-$query_posts = new Query_Posts( $atts );
-$output = $query_posts->output();
-// $output = '</ul>';
-return $output;
-
-}
-add_shortcode( 'query_posts', 'query_posts_shortcode' );
-
-// var_dump($scanned_directory = array_diff( scandir( ITALYSTRAP_PLUGIN_PATH . 'options' ), array('..', '.') ));
