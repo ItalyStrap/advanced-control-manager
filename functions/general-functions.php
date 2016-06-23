ecc3185c56446711a61b98744d2a72644024c4b4
overclokk <info@overclokk.net>
2016-06-23 14:53:25 +0200

    New function for html attributes

--- functions/general-functions.php
+++ functions/general-functions.php
@@ -270,6 +270,70 @@ function register_widget( $widget ) {
 	$wp_widget_factory->widgets[ get_class( $widget ) ] = $widget;
 }
 
+if ( ! function_exists( 'ItalyStrap\Core\get_attr' ) ) {
+
+	/**
+	 * Build list of attributes into a string and apply contextual filter on string.
+	 *
+	 * The contextual filter is of the form `italystrap_attr_{context}_output`.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @see In general-function on the plugin.
+	 *
+	 * @param  string $context    The context, to build filter name.
+	 * @param  array  $attributes Optional. Extra attributes to merge with defaults.
+	 * @param  bool   $echo       True for echoing or false for returning the value.
+	 *                            Default false.
+	 * @param  null   $args       Optional. Extra arguments in case is needed.
+	 *
+	 * @return string String of HTML attributes and values.
+	 */
+	function get_attr( $context, array $attr = array(), $echo = false, $args = null ) {
+
+		$html = '';
+
+		/**
+		 * This filters the array with html attributes.
+		 *
+		 * @var array
+		 */
+		$attr = (array) apply_filters( "italystrap_{$context}_attr", $attr, $context, $args );
+
+		foreach ( $attr as $key => $value ) {
+
+			if ( empty( $value ) ) {
+				continue;
+			}
+
+			if ( true === $value ) {
+
+				$html .= esc_html( $key ) . ' ';
+			} else {
+
+				$html .= sprintf(
+					' %s="%s"',
+					esc_html( $key ),
+					( 'href' === $key ) ? esc_url( $value ) : esc_attr( $value )
+				);
+			}
+		}
+
+		/**
+		 * This filters the output of the html attributes. 
+		 *
+		 * @var string
+		 */
+		$html = apply_filters( "italystrap_attr_{$context}_output", $html, $attr, $context, $args );
+
+		if ( ! $echo ) {
+			return $html;
+		}
+
+		echo $html;
+	}
+}
+
 /**
  * Cambio il testo per il link al post successivo
  *
