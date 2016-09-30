59effc2b5f6293d625aa115d36df999576a9588f
overclokk <info@overclokk.net>
2016-09-30 08:54:43 +0200

    Added some css class

--- templates/content-vcard.php
+++ templates/content-vcard.php
@@ -5,7 +5,7 @@
  * @package ItalyStrap
  */
 
-namespace ItalyStrap;
+namespace ItalyStrap\Core;
 
 /**
  * HTML attribute for ul container
@@ -14,17 +14,17 @@ namespace ItalyStrap;
  */
 $ul_attr = array(
 	'id'	=> 'schema',
-	'class'	=> 'list-unstyled schema' . esc_attr( $this->args['container_class'] ),
+	'class'	=> 'list-unstyled schema vCard' . esc_attr( $this->args['container_class'] ),
 	'itemtype'	=> 'http://schema.org/' . esc_textarea( $this->args['schema'] ),
 	'itemscope'=> true,
 	);
 
 ?>
 
-<ul <?php Core\get_attr( 'vcard_container', $ul_attr, true ); ?>>
+<ul <?php get_attr( 'vcard_container', $ul_attr, true ); ?>>
 
 <?php if ( $this->args['show_logo'] && $this->args['logo_id'] ) : ?>
-	<li>
+	<li class='image'>
 		<figure class="vcard-logo">
 			<?php
 			$attr = array(
@@ -46,14 +46,14 @@ $ul_attr = array(
 
 <?php endif; ?>
 
-<li>
+<li class="name">
 	<strong>
 		<a itemprop="url" href="<?php echo home_url( '/' ); // XSS ok. ?>">
 			<span itemprop="name"><?php $this->get_company_name( true ); ?></span>
 		</a>
 	</strong>
 </li>
-<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+<div class="address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
 	<li itemprop="streetAddress">
 		<?php echo esc_textarea( $this->args['street_address'] ); ?>
 	</li>
