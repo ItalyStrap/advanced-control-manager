7233e3adad55bef7d8844acfd1663062557a9900
overclokk <info@overclokk.net>
2016-11-10 12:52:07 +0100

    Future inclusion

---
+++ src/settings/src/Config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Config API Class
+ *
+ * I like this idea from Alain Schlesser https://github.com/schlessera/better-settings-v1 to have a class for managing configuration files
+ *
+ * @link https://github.com/schlessera/better-settings-v1
+ * @since 2.x.x
+ *
+ * @package [Plugin/Theme/Etc]
+ */
+
