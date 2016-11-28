26947ab99eb077749d27800feacc634faddecd44
overclokk <info@overclokk.net>
2016-11-28 22:41:21 +0100

    Added new action for widget area position

--- admin/Register_Metaboxes.php
+++ admin/Register_Metaboxes.php
@@ -137,18 +137,27 @@ class Register_Metaboxes {
 		);
 
 		$options = array(
-			'italystrap_content_header'	=> __( 'In the header', 'italystrap' ),
+			'italystrap_before'			=> __( 'After the &lt;body&gt;', 'italystrap' ),
+
+			'italystrap_before_header'	=> __( 'Before the header', 'italystrap' ),
+			'italystrap_content_header'	=> __( 'The content header', 'italystrap' ),
+			'italystrap_after_header'	=> __( 'After the header', 'italystrap' ),
+
 			'italystrap_before_main'	=> __( 'Before the Main Content', 'italystrap' ),
 			'italystrap_before_content'	=> __( 'Before the Content', 'italystrap' ),
 
 			'italystrap_before_loop'	=> __( 'Before the Loop', 'italystrap' ),
+			'italystrap_loop'			=> __( 'The Loop', 'italystrap' ),
 			'italystrap_after_loop'		=> __( 'After the Loop', 'italystrap' ),
 
-			'italystrap_after_content'		=> __( 'After the Content', 'italystrap' ),
+			'italystrap_after_content'	=> __( 'After the Content', 'italystrap' ),
+
 			'italystrap_after_main'		=> __( 'After the Main Content', 'italystrap' ),
-			'italystrap_before_footer'		=> __( 'In the footer open', 'italystrap' ),
-			'italystrap_footer'		=> __( 'In the footer', 'italystrap' ),
-			'italystrap_after_footer'		=> __( 'In the footer closed', 'italystrap' ),
+			'italystrap_before_footer'	=> __( 'In the footer open', 'italystrap' ),
+			'italystrap_footer'			=> __( 'In the footer', 'italystrap' ),
+			'italystrap_after_footer'	=> __( 'In the footer closed', 'italystrap' ),
+
+			'italystrap_after'			=> __( 'At the end of the page before the <code>&lt;/body&gt;</code>', 'italystrap' ),
 		);
 
 		$options = apply_filters( 'italystrap_widget_area_position', $options );
