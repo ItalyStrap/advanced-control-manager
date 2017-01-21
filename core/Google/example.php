fb4d7df466de91645ad5ead6ff3fa63edf0cd3da
overclokk <info@overclokk.net>
2017-01-21 14:58:05 +0100

    Added code example for GA

---
+++ core/Google/example.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Example code for Analytics
+ *
+ * @link italystrap.com
+ * @since 4.0.0
+ *
+ * @package ItalyStrap
+ */
+
+
+
+/**
+ * Aggiungo i comandi ga che prima erano presenti nel file header.
+ *
+ * This will create:
+ * ga("create","UA-xxxxxxxx-xx",{"cookieDomain":"italystrap.com","siteSpeedSampleRate":100});
+ * ga("require","linkid","linkid.js");
+ * ga("require","displayfeatures");
+ * ga("send","pageview");
+ */
+add_filter( 'italystrap_ga_commands_queue', function ( $parameters, $settings ) {
+
+    $new_commands_queue = array(
+            array(
+                'command'       => 'create',
+                'fields'        => esc_js( $settings['google_analytics_id'] ),
+                'fields_object' => array(
+                     'cookieDomain'          => 'italystrap.com',
+                     'siteSpeedSampleRate'   => 100,
+                 ),
+            ),
+            array(
+                'command'       => 'require',
+                'fields'        => 'linkid',
+                'fields_object' => 'linkid.js',
+            ),
+            array(
+                'command'       => 'require',
+                'fields'        => 'displayfeatures',
+            ),
+            array(
+                'command'       => 'send',
+                'fields'        => 'pageview',
+            ),
+    );
+
+    return $new_commands_queue;
+}, 10, 2 );
