f5209d24e09db8ebeac58bbf08247daba11819ab
overclokk <info@overclokk.net>
2017-03-20 22:46:21 +0100

    Deprecated the old Widget namespace

--- deprecated/Widget.php
+++ deprecated/Widget.php
@@ -19,4 +19,21 @@ use InvalidArgumentException;
 
 class Widget extends Old_Widget {
 
+
+	protected function create_widget( array $args ) {
+		_deprecated_function( __CLASS__, '2.5', 'ItalyStrap\\\Widgets\\\Widget' );
+		parent::create_widget( $args );
+	}
+
+	/**
+	 * Dispay the widget content
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 *
+	 * @param  array $args     Display arguments including 'before_title', 'after_title',
+	 *                        'before_widget', and 'after_widget'.
+	 * @param  array $instance The settings for the particular instance of the widget.
+	 */
+	public function widget_render( $args, $instance ){}
 }
