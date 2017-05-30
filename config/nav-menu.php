e9fb3228ee6308153d4ef1c5c187b71317030fda
overclokk <info@overclokk.net>
2017-05-31 00:12:54 +0200

    Added Widget Nav_Menu in Beta version

---
+++ config/nav-menu.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Array definition for Image widget and shortcode
+ *
+ * @package ItalyStrap
+ */
+
+if ( ! defined( 'ABSPATH' ) or ! ABSPATH ) {
+	die();
+}
+
+/**
+ * Get menus
+ *
+ * @var array
+ */
+$menus = wp_get_nav_menus();
+$registered_menu = array( '0' => __( '&mdash; Select &mdash;' ) );
+foreach ( $menus as $menu ) {
+	$registered_menu[ $menu->term_id ] = $menu->name;
+}
+
+/**
+ * Definition array() with all the options connected to the
+ * module which must be called by an include (setoptions).
+ */
+return array(
+
+	/**
+	 * Enter a font icon css class. Example:<code>fa fa-wordpress</code>. This works only if you have font icons loaded with your theme. If you use an icon the image will not be loaded.
+	 */
+	'nav_menu'				=> array(
+		'name'		=> __( 'Select Menu', 'italystrap' ),
+		'desc'		=> __( '.', 'italystrap' ),
+		'id'		=> 'nav_menu',
+		'type'		=> 'select',
+		'class'		=> 'widefat nav_menu',
+		// 'class-p'	=> 'hidden',
+		'default'	=> false,
+		'options'	=> $registered_menu,
+		// 'validate'	=> 'numeric_comma',
+		'sanitize'	=> 'sanitize_text_field',
+		'section'	=> 'general',
+	),
+);
