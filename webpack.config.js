22a1b64896120948eaf2ca79174c5aeb2ac7c76b
overclokk <info@overclokk.net>
2018-12-20 21:15:12 +0100

    Updated webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -30,26 +30,26 @@
 var webpack = require( 'webpack' ),
 	NODE_ENV = process.env.NODE_ENV || 'development',
 	webpackConfig = {
-	entry: './src/Blocks/blocks.js',
-	output: {
-		path: __dirname,
-		filename: './src/Blocks/blocks.build.js',
-	},
-	module: {
-		loaders: [
-			{
-				test: /.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-	plugins: [
-		new webpack.DefinePlugin( {
-			'process.env.NODE_ENV': JSON.stringify( NODE_ENV )
-		} ),
-	]
-};
+		entry: './src/Blocks/index.js',
+		output: {
+			path: __dirname,
+			filename: './src/Blocks/index.build.js',
+		},
+		module: {
+			loaders: [
+				{
+					test: /.js$/,
+					loader: 'babel-loader',
+					exclude: /node_modules/,
+				},
+			],
+		},
+		plugins: [
+			new webpack.DefinePlugin( {
+				'process.env.NODE_ENV': JSON.stringify( NODE_ENV )
+			} ),
+		]
+	};
 
 if ( 'production' === NODE_ENV ) {
 	webpackConfig.plugins.push( new webpack.optimize.UglifyJsPlugin() );
