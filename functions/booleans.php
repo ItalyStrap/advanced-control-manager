b83e1ba6aec61eddeacb011631342b6715f85e35
Enea <info@overclokk.net>
2020-10-02 09:17:54 +0200

    Wrapped with function_exists

--- functions/booleans.php
+++ functions/booleans.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * Functions for notices in admin panel
- *
- * @link italystrap.com
- * @since 2.0.0
- *
- * @package Italystrap\Core
- */
+declare(strict_types=1);
 
 namespace ItalyStrap\Core;
 
@@ -18,57 +11,57 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
- * Is dev enviroment
- *
- * @return bool Return true if ITALYSTRAP_DEV constant is declared
+ * @return bool
  */
-function is_dev() {
-	return (bool) defined( 'ITALYSTRAP_DEV' ) && ITALYSTRAP_DEV;
+if ( ! function_exists( __NAMESPACE__ . '\is_dev' ) ) {
+	function is_dev(): bool {
+		return (bool) defined( 'ITALYSTRAP_DEV' ) && ITALYSTRAP_DEV;
+	}
 }
 
 /**
- * Is Beta version
- *
- * @return bool Return true if ITALYSTRAP_BETA version is declared
+ * @return bool
  */
-function is_beta() {
-	return (bool) defined( 'ITALYSTRAP_BETA' ) && ITALYSTRAP_BETA;
+if ( ! function_exists( __NAMESPACE__ . '\is_beta' ) ) {
+	function is_beta(): bool {
+		return (bool) defined( 'ITALYSTRAP_BETA' ) && ITALYSTRAP_BETA;
+	}
 }
 
 /**
- * Is Beta version
- *
- * @return bool Return true if ITALYSTRAP_BETA version is declared
+ * @return bool
  */
-function is_debug() {
-	return (bool) defined( 'WP_DEBUG' ) && WP_DEBUG;
+if ( ! function_exists( __NAMESPACE__ . '\is_debug' ) ) {
+	function is_debug(): bool {
+		return (bool) defined( 'WP_DEBUG' ) && WP_DEBUG;
+	}
 }
 
 /**
- * Is Beta version
- *
- * @return bool Return true if ITALYSTRAP_BETA version is declared
+ * @return bool
  */
-function is_script_debug() {
-	return (bool) defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG;
+if ( ! function_exists( __NAMESPACE__ . '\is_script_debug' ) ) {
+	function is_script_debug(): bool {
+		return (bool) defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG;
+	}
 }
 
 /**
- * is_p2p_register_connection_type_exists
- *
- * @return bool Return true if is_p2p_register_connection_type_exists
+ * @return bool
  */
-function is_p2p_register_connection_type_exists() {
-	return (bool) function_exists( 'p2p_register_connection_type' );
+if ( ! function_exists( __NAMESPACE__ . '\is_p2p_register_connection_type_exists' ) ) {
+	function is_p2p_register_connection_type_exists(): bool {
+		return (bool) function_exists( 'p2p_register_connection_type' );
+	}
 }
 
 /**
- * Is ItalyStrap Framework active
- *
  * @todo also try this wp_get_theme( 'italystrap' )->exists()
  *
- * @return bool Return true if the ItalyStrap theme framweork is active
+ * @return bool
  */
-function is_italystrap_active() {
-	return 'italystrap' === wp_get_theme()->template;
+if ( ! function_exists( __NAMESPACE__ . '\is_italystrap_active' ) ) {
+	function is_italystrap_active(): bool {
+		return 'italystrap' === wp_get_theme()->template;
+	}
 }
